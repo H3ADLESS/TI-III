53e67e28f28dd99d0412aa2b38ddb1dbeaab1476
justayak <justayak@googlemail.com>
2013-10-29 14:30:35 +0100

    done

--- uebung02/aufgabe4.c
+++ uebung02/aufgabe4.c
@@ -20,72 +20,74 @@ BGGF
 #include <stdlib.h>
 
 void readfile(char*);
-char encdec(char, char, bool);
-char asciiToVignere(char, bool);
+void writefile(char*, char*);
+char asciiToVignere(char);
+char vignereToAscii(char);
+char absq(char);
 
 char in[INPUT_SIZE];
 
 int main(int argc, char* *argv){
-	
-	char* leaver = argv[1];
+	char* leaver = argv[1]; if (leaver[0] != '-'){
+		printf("nope..");
+		return -1;
+	}
 	char* key = argv[2]; // We expect the input to be valid: means: only capital letters and numbers!
 	char* inFile = argv[3];
 	char* outFile = argv[4];
-	
 	readfile(inFile);	
-	
-	bool encrypt = TRUE;
-	
-	// Clean input text and convert to Vignere
+	bool encrypt = FALSE;
+	if (leaver[1]=='e')encrypt = TRUE;
+	// clean input value and apply vignere
 	int i; int j; int x;
-	for(i = 0, j = 0, x = 0; in[i]; i++){
-		char current = asciiToVignere(in[i], encrypt);
-		if(current != NULL){
-			in[j] = encdec(current, &key[x], encdec); j++;
-			if(!key[x++]){
+    for(i = 0, j = 0, x = 0; in[i]; i++){
+		if ((in[i] > 96 && in[i] < 123) || 	//'a - z'
+			(in[i] > 64 && in[i] < 91) ||  	//'A - Z'
+			(in[i] > 47 && in[i] < 58)){	//'0 - 9'
+			if(encrypt){
+				in[j] = vignereToAscii(
+					( asciiToVignere(in[i]) + asciiToVignere(key[x]) ) % 36);
+			}else{
+				in[j] = vignereToAscii(
+					( absq(asciiToVignere(in[i]) - asciiToVignere(key[x]))) % 36);
+			}
+			x++;
+			if (!key[x]){
 				x = 0;
 			}
-		}
-	
-	/*
-		if (in[i] > 96 && in[i] < 123){
-			in[j] = in[i] - 32 -'A';j++;
-		}else if(in[i] > 64 && in[i] < 91){
-			in[j] = in[i] - 'A';j++;
-		}else if(in[i] > 47 && in[i] < 58){
-			in[j] = in[i] - 22;j++;
-		}
-		*/
+			j++;
+		}		
 	}
-	in[j] = '\0';
-	
-	
-	
-	//printf("stuff: %s | i:%d + j:%d\n", in, i, j);
-	
+	in[j] = '\0';	
+	writefile(outFile, in);
 	return 0;
-};
-
-char encdec(char c, char key,  bool encrypt){
-	if (encrypt)return c + asciiToVignere(key, encrypt);
-	return c - asciiToVignere(key, encrypt);
 }
 
-char asciiToVignere(char c, bool encrypt){
-	int a;int b;int d;
-	if(encrypt){
-		a = -32 - 'A'; b = -'A'; d = -22;
-	}else{
-		a = +32 + 'A'; b = +'A'; d = +22;
-	}
-	if(c > 96 && c < 123) return c + a;
-	if(c > 64 && c < 91) return c + b;
-	if(c > 47 && c < 58) return c + d;
-	return NULL;
+char asciiToVignere(char c){
+	if(c > 96 && c < 123) return c - 32 - 'A';
+	if(c > 64 && c < 91) return c - 'A';
+	return c - 22;
+}
+
+char vignereToAscii(char c){
+	if(c >= 0 && c < 26) return c + 'A';
+	return c + 22;
 }
 
 void readfile(char* filename){
-	int n;	
 	FILE *fr = fopen(filename, "rt");
 	while(fgets(in, INPUT_SIZE, fr) != NULL);
 } 
+
+void writefile(char* filename, char* text){
+	FILE *fp = fopen(filename, "w");
+	if(fp){
+		fprintf(fp, text);
+		fclose(fp);
+	}
+}
+
+char absq(char c){
+	if (c > 0) return c;
+	return c * (-1);
+}
